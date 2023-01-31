cc66d04e5e21652ca69bc7e338d41bb2b0f2474d
jferguson47 <73237765+jferguson47@users.noreply.github.com>
2023-01-31 21:06:43 +0000

    Update Question1E_reduced.m

--- Question1E_reduced.m
+++ Question1E_reduced.m
@@ -1,2 +1,5 @@
 sys = tf(25,[1 6 25]) % TRANSFER FUNCTION of Reduced System
-bode(sys) % Plotting bode plot of reduced order system
+bode(sys, opts) % Plotting bode plot of reduced order system
+grid on; title('Bode Plot of Reduced Order System');
+opts = bodeoptions; %bode options
+opts.XLim={[0.1 100]};  % setting x limits  0.1 to 100 rad/s
