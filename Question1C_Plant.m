5e03ff7d64d82675ac537c64ad5af1e1112d162d
jferguson47 <73237765+jferguson47@users.noreply.github.com>
2023-01-31 19:19:25 +0000

    Update Question1C_Plant.m

--- Question1C_Plant.m
+++ Question1C_Plant.m
@@ -1,4 +1,4 @@
-K=30 % assigned in question
+K=30; % assigned in question
 sys = tf(25*K,[1 1.2*K 205 25*K]) % TRANSFER FUNCTION of System
 opt = stepDataOptions('StepAmplitude',10);  % Adding a step value of 10
 step(sys,2.5,opt)   % Plotting step response over first 2.5s
