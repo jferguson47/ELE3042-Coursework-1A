ff191be98ee9985ffe8eedb6ed0482ec4a4386b2
jferguson47 <73237765+jferguson47@users.noreply.github.com>
2023-01-31 19:14:27 +0000

    Update Question1C_Plant.m

--- Question1C_Plant.m
+++ Question1C_Plant.m
@@ -1,4 +1,5 @@
-sys = tf(750,[1 36 205 750])
-opt = stepDataOptions('StepAmplitude',10);
-step(sys,2.5,opt)
-title('Step Response of Plant')
+K=30 % assigned in question
+sys = tf(25*K,[1 1.2*K 205 25*K]) % TRANSFER FUNCTION of System
+opt = stepDataOptions('StepAmplitude',10);  % Adding a step value of 10
+step(sys,2.5,opt)   % Plotting step response over first 2.5s
+title('Step Response of Plant') % naming graph 
