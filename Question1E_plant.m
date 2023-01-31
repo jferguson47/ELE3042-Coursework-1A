ed3d7dcd93f6aa733fc051cdde3557bd7faef015
jferguson47 <73237765+jferguson47@users.noreply.github.com>
2023-01-31 21:07:26 +0000

    Update Question1E_plant.m

--- Question1E_plant.m
+++ Question1E_plant.m
@@ -1,3 +1,6 @@
 K=30; % assigned in question
 sys = tf(25*K,[1 1.2*K 205 25*K]) % TRANSFER FUNCTION of System
-bode(sys)  % Plotting bode plot of system
+bodeplot(sys, opts)  % Plotting bode plot of system
+grid on; title('Bode Plot of System');
+opts = bodeoptions; %bode options
+opts.XLim={[0.1 100]};  % setting x limits  0.1 to 100 rad/s
