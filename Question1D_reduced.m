e658c539a0fa985f8ca4f05640250b70bfaa7055
jferguson47 <73237765+jferguson47@users.noreply.github.com>
2023-01-31 20:16:37 +0000

    Create Question1D_reduced.m

---
+++ Question1D_reduced.m
@@ -0,0 +1,6 @@
+K=30; % assigned in question
+sys = tf(25,[1 6 25]) % TRANSFER FUNCTION of Reduced System
+opt = stepDataOptions('StepAmplitude',10);  % Adding a step value of 10
+step(sys,2.5,opt)   % Plotting step response over first 2.5s
+title('Step Response of Reduced Order Model') % naming graph
+S = stepinfo(sys)
