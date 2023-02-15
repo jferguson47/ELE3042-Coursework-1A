aa5a4faab05450bd919d4ae847d20fdca646f457
jferguson47 <73237765+jferguson47@users.noreply.github.com>
2023-02-15 14:34:39 +0000

    Update Question2D_resulting_locus_step.m

--- Question2D_resulting_locus_step.m
+++ Question2D_resulting_locus_step.m
@@ -5,10 +5,12 @@ Plant = simplify(expand(Plant1))        % Simplifyingh and expanding Plant
 comp1 = (s+2)/(s+6);                  % OLTF of plant using symbol s
 comp = simplify(expand(comp1))        % Simplifyingh and expanding Plant   
 
-plant_comp = Plant * comp
+K1=0.984
+
+plant_comp = K1* Plant * comp
 
 
-[num,den] = numden(plant_comp);               % Obtaining num and den from plant      
+[num,den] = numden(plant_comp);          % Obtaining num and den from plant      
 num_coeff = double(coeffs(num, 'All'));  % getting coefficents of num in order
 den_coeff = double(coeffs(den, 'All'));  % getting coefficents of den in order
 
@@ -22,12 +24,12 @@ z = 0.7071;                     % setting damping ratio given in question
 sgrid(z,0)  
 
 
-K = 28.1; % from root locus graph
+K = 28.6; % from root locus graph
 H = 1; % Unity Feedback
 
 
 
-CLTF_sym = (K*Plant*comp)/(1+(K*Plant*H*comp))      % Symbolic version if CLTF
+CLTF_sym = (K1*K*Plant*comp)/(1+(K1*K*Plant*H*comp))% Symbolic version
 
 CLTF_sym = simplify(expand(CLTF_sym));          % simplifying CLTF symbolic 
 
@@ -37,12 +39,12 @@ num_coeff = double(coeffs(num, 'All')); % getting coefficents of num in order
 den_coeff = double(coeffs(den, 'All')); % getting coefficents of den in order
 
 
-CLTF = tf(num_coeff,den_coeff);
+CLTF5 = tf(num_coeff,den_coeff)
 
 
-closed_loop_poles = pole(CLTF)
+closed_loop_poles = pole(CLTF5)
 
 figure();
-step(CLTF)
+step(CLTF5)
 title("Step Response of Compensated system")
 S = stepinfo(CLTF)
